c9426e6b2176a6ef3dca14ccbdbc900b5150e3d6
Nick Palladinos <nick.palladinos@gmail.com>
2025-10-26 19:48:11 +0200

    Remove batteries package

--- lakefile.lean
+++ lakefile.lean
@@ -5,9 +5,6 @@ package «CogitoCore» where
   -- All Lean modules are relative to the repository root.
   srcDir := "."
 
-require batteries from git
-  "https://github.com/leanprover-community/batteries" @ "main"
-
 lean_lib «CogitoCore» where
   -- We expose the aggregated `CogitoCore` module as the library entry point.
   roots := #[`CogitoCore]
